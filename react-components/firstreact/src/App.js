a85d24aeb53d938de6953d8effc253cb193f89af
zain-mzb <comsatsbscs@gmail.com>
2021-04-20 00:31:08 +0500

    Initial commit

---
+++ react-components/firstreact/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+
+
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello from React Component </h1>
+    <Button>Bootstrap</Button>
+      </div>
+     );
+}
+
+export default App;
